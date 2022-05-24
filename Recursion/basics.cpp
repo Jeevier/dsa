ebc2a3a281ea3633d46c5ee51b8bb0db8fe6ed11
SanyamJain <sanyamjain10168@gmail.com>
2022-05-24 18:13:56 +0530

    more ques

--- Recursion/basics.cpp
+++ Recursion/basics.cpp
@@ -34,7 +34,7 @@ void printSub(int idx, vi& arr, vi& path, vvi& ans) {
 }
 
 // 90. Subsets II : Get all unique subsets(duplicates allowed)
-// APPROACH: 1 BY LOOPING and check duplicates by comparing adjacents
+// APPROACH: 1 BY LOOPING and check duplicates by comparing adjacents(because we do the sort first)
 void help(int idx, vector<int>& nums, vector<int>& path, vector<vector<int>>& ans) {
     ans.push_back(path);
     for (int i = idx;i < nums.size();i++) {
@@ -45,3 +45,36 @@ void help(int idx, vector<int>& nums, vector<int>& path, vector<vector<int>>& an
     }
 }
 
+//39. Combination Sum: (all unique integers)
+//# Get unique combinations of candidates where the chosen numbers sum to target 
+//# The same number may be chosen from candidates an unlimited number of times.
+void dfs(int i, int sum, vector<int>& candidates, vector<int>& path, int target, vector<vector<int>>& ans) {
+    if (target == sum) {
+        ans.push_back(path);
+        return;
+    }
+    if (i >= candidates.size() || sum > target) return;
+
+    for (int j = i;j < candidates.size();j++) {
+        path.push_back(candidates[j]);
+        dfs(j, sum + candidates[j], candidates, path, target, ans);
+        path.pop_back();
+    }
+}
+//40. Combination Sum II (duplicates allowed)
+//# All unique combinations in candidates where the candidate numbers sum to target.
+//# Each number in candidates may only be used once in the combination.
+// APPROACH: BY LOOPING and check duplicates by comparing adjacents(because we do the sort first)
+void dfs(int idx, int sum, int target, vector<int>& candidates, vector<int>& path, vector<vector<int>>& ans) {
+    if (target == sum) {
+        ans.push_back(path);
+        return;
+    }
+    for (int i = idx;i < candidates.size();i++) {
+        if (i > idx && candidates[i] == candidates[i - 1]) continue;
+        if (sum + candidates[i] > target) break;
+        path.push_back(candidates[i]);
+        dfs(i + 1, sum + candidates[i], target, candidates, path, ans);
+        path.pop_back();
+    }
+}
